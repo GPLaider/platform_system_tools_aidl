86e7e9feab1397746e8cbadedd533d56aa40ec9f
OSverflow <osverflow@users.noreply.github.com>
2026-09-19 12:15:46 +0900

    OSverflow lyriq: binder generator compatibility fix
    Change-Id: I42172e05fd9ad16c49424328f906b4953342ffc4

--- generate_java_binder.cpp
+++ generate_java_binder.cpp
@@ -387,19 +387,21 @@ class ProxyClass : public Class {
 };
 
 ProxyClass::ProxyClass(const AidlInterface* interfaceType, const Options& options) : Class() {
-  this->modifiers = PRIVATE | STATIC;
+  this->modifiers = PUBLIC | STATIC;
+  this->comment = "/** @hide */";
   this->what = Class::CLASS;
   this->type = interfaceType->GetCanonicalName() + ".Stub.Proxy";
   this->interfaces.push_back(interfaceType->GetCanonicalName());
 
   // IBinder mRemote
   mRemote = std::make_shared<Variable>("android.os.IBinder", "mRemote");
-  this->elements.push_back(std::make_shared<Field>(PRIVATE, mRemote));
+  this->elements.push_back(std::make_shared<Field>(PROTECTED, mRemote));
 
   // Proxy()
   auto remote = std::make_shared<Variable>("android.os.IBinder", "remote");
   auto ctor = std::make_shared<Method>();
   ctor->name = "Proxy";
+  ctor->modifiers = PROTECTED;
   ctor->statements = std::make_shared<StatementBlock>();
   ctor->parameters.push_back(remote);
   ctor->statements->Add(std::make_shared<Assignment>(mRemote, remote));
@@ -919,8 +921,9 @@ static void GenerateMethods(const AidlInterface& iface, const AidlMethod& method
   string transactCodeName = "TRANSACTION_";
   transactCodeName += method.GetName();
 
-  auto transactCode =
-      std::make_shared<Field>(STATIC | FINAL, std::make_shared<Variable>("int", transactCodeName));
+  auto transactCode = std::make_shared<Field>(
+      PUBLIC | STATIC | FINAL, std::make_shared<Variable>("int", transactCodeName));
+  transactCode->comment = "/** @hide */";
   transactCode->value =
       StringPrintf("(android.os.IBinder.FIRST_CALL_TRANSACTION + %d)", index);
   stubClass->elements.push_back(transactCode);
